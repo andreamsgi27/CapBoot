6e4cfb8f60d3538b6a4252e25ae9b8b72b0717cf
andreamsgi27 <andreamsgi27@hotmail.com>
2025-03-07 09:32:25 +0100

    catalogo app

--- catalogo/demo/src/main/java/com/example/demo/CatalogoApplication.java
+++ catalogo/demo/src/main/java/com/example/demo/CatalogoApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class CatalogoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CatalogoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.err.println("Aplicación pruebaspringboot arrancada");
+		System.err.println("Catalogo arrancado");
 	}
+
 }
