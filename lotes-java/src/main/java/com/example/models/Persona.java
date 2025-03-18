71e8157aba5d9ab9951f2b84568543cf402b0e12
andreamsgi27 <andreamsgi27@hotmail.com>
2025-03-18 16:28:19 +0100

    New batch project

---
+++ lotes-java/src/main/java/com/example/models/Persona.java
@@ -0,0 +1,13 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Persona {
+	private long id;
+	private String nombre, correo, ip;
+	// Ctor(), Get/Set, toString(), …
+}
+
