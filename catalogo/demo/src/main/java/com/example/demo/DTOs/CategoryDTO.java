4358bf7b65972e726b27d439c8f8dbe93469ee91
andreamsgi27 <andreamsgi27@hotmail.com>
2025-03-24 10:55:45 +0100

    fixeado GetOne del controller de categoria

--- catalogo/demo/src/main/java/com/example/demo/DTOs/CategoryDTO.java
+++ catalogo/demo/src/main/java/com/example/demo/DTOs/CategoryDTO.java
@@ -1,5 +1,11 @@
 package com.example.demo.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
 public class CategoryDTO {
-    
+    private int categoryId;
+    private String name;
 }
