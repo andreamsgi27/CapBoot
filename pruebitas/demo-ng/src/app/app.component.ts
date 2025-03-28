93d8d48fe15bf1f0100c1120d8044719ae21dd20
andreamsgi27 <andreamsgi27@hotmail.com>
2025-03-28 13:27:12 +0100

    laboratorio de angular primera parte hasta verificación sin errores

--- pruebitas/demo-ng/src/app/app.component.ts
+++ pruebitas/demo-ng/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService} from '@my/core';
+import { NotificationComponent } from './main/notification/notification.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:  [CommonModule, RouterOutlet, NotificationComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ pruebitas/demo-ng/src/app/index.ts
@@ -0,0 +1,1 @@
+export * from './main/notification/notification.component';
