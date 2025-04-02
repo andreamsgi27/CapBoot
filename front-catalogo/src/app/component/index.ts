658a3c42c94c02bd64ba3db97143757795e6e5ce
andreamsgi27 <andreamsgi27@hotmail.com>
2025-04-02 17:00:48 +0200

    Comienzo del proyecto final con front

---
+++ front-catalogo/src/app/component/index.ts
@@ -0,0 +1,4 @@
+export * from "./actors/actors.component"
+export * from "./categories/categories.component"
+export * from "./films/films.component"
+export * from "./languages/languages.component"
